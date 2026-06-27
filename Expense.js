2685565c4a23dc0a3f46295d2f25dde5ce9f1e05
Krusha Vadher <krushavadher14@gmail.com>
2026-06-27 12:00:48 +0530

    Add files via upload

---
+++ Expense.js
@@ -0,0 +1,49 @@
+name1=document.querySelector(".name1")
+price=document.querySelector(".price")
+btnadd=document.querySelector(".add")
+
+const arr=[];
+
+btnadd.addEventListener("click",()=>{
+    name=name1.value;
+    amount=Number(price.value);
+    arr.push(
+       {
+         name:name,
+        amount:amount
+       }
+    );
+
+
+    console.log(arr);
+    
+})
+
+showbtn=document.querySelector(".showbtn")
+
+items=document.querySelector(".items")
+
+showbtn.addEventListener("click",()=>{
+    arr.forEach(element => {
+        const cnode=document.createElement("p");
+        cnode.textContent=`${element.name} : ${element.amount}`
+        
+        items.append(cnode)
+        
+    });
+})
+
+totalbtn=document.querySelector(".totalbtn")
+sum=0;
+calculate=document.querySelector(".calculate")
+
+totalbtn.addEventListener("click",()=>{
+    arr.forEach(element => {
+        sum+=element.amount;
+    });
+    const cnode2=document.createElement("p");
+    cnode2.textContent=`total is ${sum}`;
+    calculate.append(cnode2)
+})
+
+
